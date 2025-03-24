e91296dc2ba0519a99f467f217de3016c0b866f9
Erwan Mayolle <erwan.mayolle.pro@gmail.com>
2025-03-24 17:10:20 +0100

    (refactor) Improve initialization in main.cpp and define baud rate constant

--- src/main.cpp
+++ src/main.cpp
@@ -7,25 +7,27 @@
 #include "SIM7080G_GNSS.hpp"
 #include "SIM7080G_CATM1.hpp"
 
-#define PARAMS(...) __VA_ARGS__
-
-uint32_t last_time = 0;
+const uint32_t BAUD_RATE = 115200;
 
 void setup()
 {
+	// Initialize pins
 	pinMode(PWR_KEY, OUTPUT);
 
-	Serial.begin(115200);
+	// Initialize serial communication
+	Serial.begin(BAUD_RATE);
 	Serial.println("[+] Start Up");
 
+	// Initialize SIM7080G module
 	Sim7080G.begin(Sim7080G_BAUDRATE, SERIAL_8N1, RX0, TX0);
 	Sim7080G.flush(true);
 
+	// Set initial state
 	SET_STATE(STARTING);
-	last_time = millis();
 }
 
 void loop()
 {
+	// Run finite state machine
 	SIM7080G::FSM();
 }
